de53b0573d48ce397a898e2f581e0598b1d8df30
Nareshcodes <nareshcodes@gmail.com>
2025-07-08 19:02:14 +0545

    debugged semester model

--- app/Http/Controllers/api/AdminController.php
+++ app/Http/Controllers/api/AdminController.php
@@ -27,7 +27,7 @@ class AdminController extends Controller
     }
     public function semester(){
         $semesters = semester::all();
-        if(!empty($semester)){
+        if(!empty($semesters)){
 
         return ResourcesSemester::collection($semesters);
         }
