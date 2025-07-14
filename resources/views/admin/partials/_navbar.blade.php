9961a605ddf2583e5b3aa6b2ea4b1e8efa0d56ba
Nareshcodes <nareshcodes@gmail.com>
2025-07-14 22:32:52 +0545

    working on backend debugging errors and fixing design

--- resources/views/admin/partials/_navbar.blade.php
+++ resources/views/admin/partials/_navbar.blade.php
@@ -49,20 +49,13 @@
                                     </div>
                                 </div>
                                 <div class="flex-grow-1">
-                                    <span class="fw-semibold d-block">John Doe</span>
-                                    <small class="text-muted">Admin</small>
+                                    <span class="fw-semibold d-block">{{ Auth::user()->name }}</span>
                                 </div>
                             </div>
                         </a>
                     </li>
                     <li>
                         <div class="dropdown-divider"></div>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">
-                            <i class="bx bx-user me-2"></i>
-                            <span class="align-middle">My Profile</span>
-                        </a>
                     </li>
                     <li>
                         <a class="dropdown-item" href="#">
